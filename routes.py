d7ce46404266d7ebe9dc77af04eb5e9106461443
antonzubikov <124088976+antonzubikov@users.noreply.github.com>
2023-07-17 12:39:09 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
 from marshmallow import ValidationError
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.flask import FlaskPlugin
+from flasgger import APISpec, Swagger
+from werkzeug.serving import WSGIRequestHandler
 
 from models import (
     BOOKS_DATA,
@@ -21,13 +25,52 @@ from schemas import BookSchema, AuthorSchema
 app = Flask(__name__)
 api = Api(app)
 
+spec = APISpec(
+    title='BookList',
+    version='1.0.0',
+    openapi_version='2.0',
+    plugins=[
+        FlaskPlugin(),
+        MarshmallowPlugin(),
+    ],
+)
+
 
 class BookList(Resource):
     def get(self) -> tuple[list[dict], int]:
+        """
+        This is an endpoint for obtaining the books list.
+        ---
+        tags:
+          - books
+        responses:
+          200:
+            description: Books data
+            schema:
+              type: array
+              items:
+                $ref: '#/definitions/Book'
+        """
         schema = BookSchema()
         return schema.dump(get_all_books(), many=True), 200
 
     def post(self) -> tuple[dict, int]:
+        """
+        This is an endpoint for book creation.
+        ---
+        tags:
+         - books
+        parameters:
+         - in: body
+           name: new book params
+           schema:
+             $ref: '#/definitions/Book'
+        responses:
+         201:
+           description: The book has been created
+           schema:
+             $ref: '#/definitions/Book'
+        """
         data = request.json
         schema = BookSchema()
         try:
@@ -41,6 +84,22 @@ class BookList(Resource):
 
 class Book_by_ID(Resource):
     def put(self, id: int) -> tuple[list[dict], int]:
+        """
+        This is an endpoint for book update.
+        ---
+        tags:
+         - books
+        parameters:
+         - in: body
+           name: update book params
+           schema:
+             $ref: '#/definitions/Book'
+        responses:
+         200:
+           description: The book has been update
+           schema:
+             $ref: '#/definitions/Book'
+        """
         data = request.json
         schema = BookSchema()
         try:
@@ -52,10 +111,36 @@ class Book_by_ID(Resource):
         return schema.dump(get_all_books(), many=True), 200
 
     def get(self, id: int) -> tuple[dict, int]:
+        """
+        This is an endpoint for obtaining the book by ID.
+        ---
+        tags:
+          - books
+        responses:
+          200:
+            description: Book by ID
+            schema:
+              type: array
+              items:
+                $ref: '#/definitions/Book'
+        """
         schema = BookSchema()
         return schema.dump(get_book_by_id(id)), 200
 
     def delete(self, id: int) -> tuple[list[dict], int]:
+        """
+        This is an endpoint for delete the book by ID.
+        ---
+        tags:
+          - books
+        responses:
+          200:
+            description: Delete book by ID
+            schema:
+              type: array
+              items:
+                $ref: '#/definitions/Book'
+        """
         schema = BookSchema()
         delete_book_by_id(id)
         return schema.dump(get_all_books(), many=True), 200
@@ -63,6 +148,22 @@ class Book_by_ID(Resource):
 
 class AuthorCreate(Resource):
     def post(self) -> tuple[dict, int]:
+        """
+        This is an endpoint for author creation.
+        ---
+        tags:
+         - authors
+        parameters:
+         - in: body
+           name: new author creation
+           schema:
+             $ref: '#/definitions/Author'
+        responses:
+         201:
+           description: The author has been created
+           schema:
+             $ref: '#/definitions/Author'
+        """
         data = request.json
         schema = AuthorSchema()
         try:
@@ -76,21 +177,55 @@ class AuthorCreate(Resource):
 
 class AuthorManipulate(Resource):
     def get(self, id: int) -> tuple[list[dict], int]:
+        """
+        This is an endpoint for obtaining the author by ID.
+        ---
+        tags:
+          - authors
+        responses:
+          200:
+            description: Authors data
+            schema:
+              type: array
+              items:
+                $ref: '#/definitions/Author'
+        """
         schema = AuthorSchema()
         return schema.dump(get_author_books_by_id(id), many=True), 200
 
     def delete(self, id: int) -> tuple[list[dict], int]:
+        """
+        This is an endpoint for delete the author by ID.
+        ---
+        tags:
+          - authors
+        responses:
+          200:
+            description: Delete author by ID
+            schema:
+              type: array
+              items:
+                $ref: '#/definitions/Author'
+        """
         schema = BookSchema()
         delete_author_and_books_by_author_id(id)
         return schema.dump(get_all_books(), many=True), 200
 
+
+template = spec.to_flasgger(
+    app,
+    definitions=[BookSchema],
+)
+swagger = Swagger(app, template=template)
 
 api.add_resource(BookList, '/api/books')
 api.add_resource(Book_by_ID, '/api/books/<int:id>')
 api.add_resource(AuthorCreate, '/api/authors')
 api.add_resource(AuthorManipulate, '/api/authors/<int:id>')
 
+
 if __name__ == '__main__':
     init_db_books(initial_records=BOOKS_DATA)
     init_db_authors(initial_records=AUTHORS_DATA)
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
     app.run(debug=True)
